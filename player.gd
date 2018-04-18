f44ea8e88dac7b2d3a3434b5c35de265cf3dd153
mike <humdruman@gmail.com>
2018-04-19 00:05:28 +0300

    v1

--- player.gd
+++ player.gd
@@ -8,10 +8,10 @@ var dir = Vector2()
 func _process(delta):
 	
 	if Input.is_action_pressed("ui_left"):
-		position.x -=  2
+		position.x -=  5
 		$AnimatedSprite.play();
 	elif Input.is_action_pressed("ui_right"):
-		position.x +=  2
+		position.x +=  5
 		$AnimatedSprite.play();
 	elif Input.is_action_just_pressed("ui_jump"):
 		position.y -= jump
